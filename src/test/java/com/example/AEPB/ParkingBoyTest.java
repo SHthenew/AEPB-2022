1c00eb6a64e4d2e8e7830fc74fea37aba2bb4306
JINZENG <jing.zeng@thoughtworks.com>
2022-09-21 23:32:03 +0800

    [jing zeng] feat: parking duplicate car in different parking lot

--- src/test/java/com/example/AEPB/ParkingBoyTest.java
+++ src/test/java/com/example/AEPB/ParkingBoyTest.java
@@ -38,7 +38,7 @@ public class ParkingBoyTest {
     void should_get_ticket_and_park_into_2nd_lot_when_parking_given_3_parking_lot_and_first_full_and_second_have_space_and_normal_car() {
         // given
         ParkingLot firstLot = parkingLots.get(0);
-        fillLot(firstLot);
+        fillLot(firstLot, firstLot.getMaxCapacity());
         Car car = Car.builder().plateNo(UUID.randomUUID().toString()).build();
 
         // when
@@ -51,7 +51,7 @@ public class ParkingBoyTest {
     @Test
     void should_parking_failed_when_parking_car_given_parking_lot_is_all_full() {
         // given
-        parkingLots.forEach(this::fillLot);
+        parkingLots.forEach(lot -> fillLot(lot, lot.getMaxCapacity()));
         Car car = Car.builder().plateNo(UUID.randomUUID().toString()).build();
 
         //when
@@ -75,8 +75,22 @@ public class ParkingBoyTest {
         assertEquals(car, returnedCar);
     }
 
-    private void fillLot(ParkingLot parkingLot) {
-        IntStream.range(0, parkingLot.getMaxCapacity())
+    @Test
+    void should_parking_failed_when_parking_given_1st_remain_1_space_and_2nd_empty_and_duplicated_car() {
+        fillLot(parkingLots.get(0), parkingLots.get(0).getMaxCapacity() - 1);
+        Car car = Car.builder().plateNo(UUID.randomUUID().toString()).build();
+        Ticket ticket = parkingBoy.parkingCar(car);
+
+        // when
+        ParkingCarException thrown = assertThrows(ParkingCarException.class,
+                () -> parkingBoy.parkingCar(car));
+
+        // then
+        assertEquals("have duplicated car in parking lot", thrown.getMessage());
+    }
+
+    private void fillLot(ParkingLot parkingLot, int size) {
+        IntStream.range(0, size)
                 .forEach(i -> parkingLot.parkingCar(Car.builder().plateNo(UUID.randomUUID().toString()).build()));
     }
 

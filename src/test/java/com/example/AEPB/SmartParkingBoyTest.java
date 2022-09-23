23ae3ecc44b7d74d9563a5b5211461429cf8d2bf
gejian <gejian.liao@thoughtworks.com>
2022-09-23 22:26:54 +0800

    [gejian liao] refactor: use parameterized test for smartParkingBody  .

--- src/test/java/com/example/AEPB/SmartParkingBoyTest.java
+++ src/test/java/com/example/AEPB/SmartParkingBoyTest.java
@@ -1,12 +1,21 @@
 package com.example.AEPB;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ParameterContext;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.aggregator.AggregateWith;
+import org.junit.jupiter.params.aggregator.ArgumentsAccessor;
+import org.junit.jupiter.params.aggregator.ArgumentsAggregationException;
+import org.junit.jupiter.params.aggregator.ArgumentsAggregator;
 import org.junit.jupiter.params.provider.CsvSource;
 
 import java.util.List;
 import java.util.UUID;
+import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -42,21 +51,41 @@ class SmartParkingBoyTest {
             "10, 3, 2, 2"
     })
     void should_get_ticket_when_parking_given_parked_car_number_and_normal_car(
-            int lotANumber, int lotBNumber, int lotCNumber, int parkedOrder) {
+            @AggregateWith(ParkedNumberAndParkingOrderAggregate.class) ParkedNumberAndParkingOrder parkedNumberAndParkingOrder) {
         // given
-        fillLot(parkingLots.get(0), lotANumber);
-        fillLot(parkingLots.get(1), lotBNumber);
-        fillLot(parkingLots.get(2), lotCNumber);
+        IntStream.range(0, parkingLots.size())
+                .forEach(i -> fillLot(parkingLots.get(i), parkedNumberAndParkingOrder.getParkedNumbers().get(i)));
         Car car = Car.builder().plateNo(UUID.randomUUID().toString()).build();
         // when
         Ticket ticket = smartParkingBoy.parkingCar(car);
         // then
-        assertEquals(car, parkingLots.get(parkedOrder).pickUpCar(ticket));
+        assertEquals(car, parkingLots.get(parkedNumberAndParkingOrder.getParkingOrder()).pickUpCar(ticket));
     }
 
     private void fillLot(ParkingLot parkingLot, int size) {
         IntStream.range(0, size)
                 .forEach(i -> parkingLot.parkingCar(Car.builder().plateNo(UUID.randomUUID().toString()).build()));
     }
+
+    @Getter
+    @Setter
+    @AllArgsConstructor
+    private static class ParkedNumberAndParkingOrder {
+        private final List<Integer> parkedNumbers;
+        private final Integer parkingOrder;
+
+    }
+
+    private static class ParkedNumberAndParkingOrderAggregate implements ArgumentsAggregator {
+        @Override
+        public ParkedNumberAndParkingOrder aggregateArguments(ArgumentsAccessor arguments, ParameterContext context) throws ArgumentsAggregationException {
+            List<Integer> parkedNumbers = IntStream.range(0, arguments.size() - 1)
+                    .mapToObj(arguments::getInteger)
+                    .collect(Collectors.toList());
+            Integer parkingOrder = arguments.getInteger(arguments.size() - 1);
+            return new ParkedNumberAndParkingOrder(parkedNumbers, parkingOrder);
+        }
+    }
+
 
 }

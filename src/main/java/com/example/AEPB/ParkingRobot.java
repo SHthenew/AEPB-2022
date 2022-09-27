62a27dd3b3329d8f4a69b60b2cd56661a26bf490
JINZENG <jing.zeng@thoughtworks.com>
2022-09-27 23:04:33 +0800

    [jing zeng] refactor: use ParkingCar and PickUpCar interface for robot

--- src/main/java/com/example/AEPB/ParkingRobot.java
+++ src/main/java/com/example/AEPB/ParkingRobot.java
@@ -4,21 +4,39 @@ import java.util.Comparator;
 import java.util.List;
 import java.util.Optional;
 
-public class ParkingRobot extends ParkingRole {
+public class ParkingRobot implements ParkingCar, PickUpCar {
+
+    private final List<ParkingLot> parkingLots;
 
     public ParkingRobot(List<ParkingLot> parkingLots) {
-        super(parkingLots);
+        this.parkingLots = parkingLots;
     }
 
     @Override
-    protected Optional<ParkingLot> pickParkingLot() {
-        return parkingLots.stream()
-                .filter(ParkingLot::haveCapacity)
-                .max(Comparator.comparingDouble(ParkingLot::emptyRatio));
+    public Ticket parkingCar(Car car) {
+        if (containCar(car)) {
+            throw new ParkingCarException("have duplicated car in parking lot");
+        }
+
+        ParkingLot parkingLot = pickParkingLot()
+                .orElseThrow(() -> new ParkingCarException("all parking lots is full"));
+
+        return parkingLot.parkingCar(car);
     }
 
     @Override
     public Car pickUp(Ticket ticket) {
         throw new PickUpException("robot can not pick up");
+    }
+
+    private Optional<ParkingLot> pickParkingLot() {
+        return parkingLots.stream()
+                .filter(ParkingLot::haveCapacity)
+                .max(Comparator.comparingDouble(ParkingLot::emptyRatio));
+    }
+
+    private boolean containCar(Car car) {
+        return parkingLots.stream()
+                .anyMatch(lot -> lot.containCar(car));
     }
 }

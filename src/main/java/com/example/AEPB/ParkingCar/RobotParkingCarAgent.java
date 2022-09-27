7c4461f28c63a85a350c6d3079d645c9c860c25b
gejian <gejian.liao@thoughtworks.com>
2022-09-28 00:03:15 +0800

    [gejian liao] refactor: introduce ParkingCarAgentBase.

--- src/main/java/com/example/AEPB/ParkingCar/RobotParkingCarAgent.java
+++ src/main/java/com/example/AEPB/ParkingCar/RobotParkingCarAgent.java
@@ -1,7 +1,6 @@
 package com.example.AEPB.ParkingCar;
 
 import com.example.AEPB.Car;
-import com.example.AEPB.ParkingCarException;
 import com.example.AEPB.ParkingLot;
 import com.example.AEPB.Ticket;
 
@@ -9,27 +8,15 @@ import java.util.Comparator;
 import java.util.List;
 import java.util.Optional;
 
-public class RobotParkingCarAgent implements ParkingCarAgent {
+public class RobotParkingCarAgent extends ParkingCarAgentBase implements ParkingCarAgent {
     @Override
     public Ticket parkingCar(Car car, List<ParkingLot> lots) {
-        if (containCar(lots, car)) {
-            throw new ParkingCarException("have duplicated car in parking lot");
-        }
-
-        ParkingLot parkingLot = pickParkingLot(lots)
-                .orElseThrow(() -> new ParkingCarException("all parking lots is full"));
-
-        return parkingLot.parkingCar(car);
+        return parkingCarBase(car, lots);
     }
 
-    private Optional<ParkingLot> pickParkingLot(List<ParkingLot> parkingLots) {
+    protected Optional<ParkingLot> pickParkingLot(List<ParkingLot> parkingLots) {
         return parkingLots.stream()
                 .filter(ParkingLot::haveCapacity)
                 .max(Comparator.comparingDouble(ParkingLot::emptyRatio));
-    }
-
-    private boolean containCar(List<ParkingLot> parkingLots, Car car) {
-        return parkingLots.stream()
-                .anyMatch(lot -> lot.containCar(car));
     }
 }
